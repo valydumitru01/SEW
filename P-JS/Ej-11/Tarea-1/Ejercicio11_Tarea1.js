f3841d4d37a08c8a24a62c097ae9a0d8b0bc411d
valen <valydumitru01@yahoo.com>
2021-11-30 21:03:38 +0100

    a

---
+++ P-JS/Ej-11/Tarea-1/Ejercicio11_Tarea1.js
@@ -0,0 +1,37 @@
+
+"use strict";
+class Geolocalizacion{
+    constructor (){
+        navigator.geolocation.getCurrentPosition(this.getPosicion.bind(this));
+    }
+    getPosicion(posicion){
+        this.longitud         = posicion.coords.longitude; 
+        this.latitud          = posicion.coords.latitude;  
+        this.precision        = posicion.coords.accuracy;
+        this.altitud          = posicion.coords.altitude;
+        this.precisionAltitud = posicion.coords.altitudeAccuracy;
+        this.rumbo            = posicion.coords.heading;
+        this.velocidad        = posicion.coords.speed;       
+    }
+    getLongitud(){
+        return this.longitud;
+    }
+    getLatitud(){
+        return this.latitud;
+    }
+    getAltitud(){
+        return this.altitud;
+    }
+    verTodo(){
+        var datos='<h2> Datos </h2>'; 
+        datos+='<p>Longitud: '+this.longitud +' grados</p>'; 
+        datos+='<p>Latitud: '+this.latitud +' grados</p>';
+        datos+='<p>Precision de la latitud y longitud: '+ this.precision +' metros</p>';
+        datos+='<p>Altitud: '+ this.altitude +' metros</p>';
+        datos+='<p>Precision de la altitud: '+ this.precisionAltitud +' metros</p>'; 
+        datos+='<p>Rumbo: '+ this.rumbo +' grados</p>'; 
+        datos+='<p>Velocidad: '+ this.velocidad +' metros/segundo</p>';
+        document.getElementById("info").innerHTML = datos;
+    }
+}
+var miPosicion = new Geolocalizacion();
