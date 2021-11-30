920e675c7aedcff8660eb0a4636e69b3b0552963
valen <valydumitru01@yahoo.com>
2021-11-30 21:50:06 +0100

    a
    a

--- P-JS/Ej-11/Tarea-4/Ejercicio11_Tarea4.js
+++ P-JS/Ej-11/Tarea-4/Ejercicio11_Tarea4.js
@@ -3,7 +3,7 @@
 class Geolocalizacion {
     initMap() {
         var oviedo = {lat: 43.3672702, lng: -5.8502461};
-        var mapaOviedo = new google.maps.Map(document.getElementsByName('mapa')[0],{zoom: 8,center:oviedo});
+        var mapaOviedo = new google.maps.Map(document.getElementsByTagName('section')[0],{zoom: 8,center:oviedo});
         var marcador = new google.maps.Marker({position:oviedo,map:mapaOviedo})
     }
 }
