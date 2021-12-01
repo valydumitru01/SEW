3a399374e039b497e93021cba5547d7cd2b80435
valen <valydumitru01@yahoo.com>
2021-12-01 02:01:07 +0100

    a

--- P-JS/Ej-2/ECMAscript/NombreNavegador.js
+++
@@ -1,1 +0,0 @@
-document.write(infoNavegador.nombre)
---
+++ P-JS/Ej-2/Tarea-6/MasInfoNavegador.js
@@ -0,0 +1,19 @@
+document.write("<p>Version: ")
+document.write(infoNavegador.version)
+document.write("</p>")
+
+document.write("<p>Plataforma: ")
+document.write(infoNavegador.plataforma)
+document.write("</p>")
+
+document.write("<p>Vendedor: ")
+document.write(infoNavegador.vendedor)
+document.write("</p>")
+
+document.write("<p>Agente de usuario: ")
+document.write(infoNavegador.agente)
+document.write("</p>")
+
+document.write("<p>Java activado: ")
+document.write(infoNavegador.javaActivo)
+document.write("</p>")
