932bf9175315d296b3b69c1716045fe9d839c32b
valen <valydumitru01@yahoo.com>
2021-12-01 03:44:38 +0100

    Update Ejercicio11_Tarea5.js

--- P-JS/Ej-11/Tarea-5/Ejercicio11_Tarea5.js
+++ P-JS/Ej-11/Tarea-5/Ejercicio11_Tarea5.js
@@ -3,7 +3,7 @@
 class Geolocalizacion {
     initMap() {
         var centro = { lat: 43.3672702, lng: -5.8502461 };
-        var mapaGeoposicionado = new google.maps.Map(document.getElementsById('mapa')[0], {
+        var mapaGeoposicionado = new google.maps.Map(document.getElementById('mapa'), {
             zoom: 8,
             center: centro,
             mapTypeId: google.maps.MapTypeId.ROADMAP
